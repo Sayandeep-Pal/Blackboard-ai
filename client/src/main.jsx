d5ba44ec6656ddb94dd837eed06656b2519209bf
Sayandeep-Pal <145487074+Sayandeep-Pal@users.noreply.github.com>
2024-11-25 13:03:31 +0530

    1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="732493616824-t61jg5fv6kte173v3bcloh04jci234op.apps.googleusercontent.com">
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+          <App />
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </GoogleOAuthProvider>
+    {/* <App /> */}
   </StrictMode>,
 )
