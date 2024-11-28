a24f343b2e743df75dc97527fedef0f9a5371989
Sayandeep-Pal <145487074+Sayandeep-Pal@users.noreply.github.com>
2024-11-28 17:20:34 +0530

    final commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,40 +7,51 @@ function App() {
   const URL = "https://blackboard-ai-be.vercel.app";
   // const URL = "http://localhost:3000";
 
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        axios.get(`${URL}/log`)
-            .then(res => setUsers(res.data))
-            .catch(err => console.log(err))
-    },[]);
+  useEffect(() => {
+    axios.get(`${URL}/log`)
+      .then(res => setUsers(res.data))
+      .catch(err => console.log(err))
+  }, []);
 
-    return (
-        <div className='container'>
-            <div className='table-container'>
-                <h1 style={{color: 'whitesmoke'}}>User Table</h1>
-                <table className='user-table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map((user) => {
-                                return <tr key={user._id}>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.createdAt}</td>
-                                </tr>
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-         </div>
+  function formatDate(isoDate) {
+    const date = new Date(isoDate);
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    }).format(date);
+  }
+
+  return (
+    <div className='container'>
+      <div className='table-container'>
+        <h1 style={{ color: 'whitesmoke' }}>User Table</h1>
+        <table className='user-table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user) => {
+                return <tr key={user._id}>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{formatDate(user.createdAt)}</td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
 
   )
 }
